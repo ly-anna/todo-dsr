0784eb2c35189c6039c9569e1bf98f744627a7b9
Annsysadm <annsysadm@mail.ru>
2020-04-27 02:41:24 +0300

    added simple form

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Form, Field } from 'react-final-form';
+import './App.css';
+
+
+const onSubmit = values => window.alert(JSON.stringify(values, 0, 2))
+
+const App = () => (
+  <Form
+    onSubmit={onSubmit}
+    render={({ handleSubmit, values }) => (
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="firstName">First Name</label>
+          <Field
+            name="firstName"
+            component="input"
+            type="text"
+            placeholder="First Name"
+          />
+        </div>
+        <div>
+          <label htmlFor="secondName">Second Name</label>
+          <Field
+            name="secondName"
+            component="input"
+            type="text"
+            placeholder="Second Name"
+          />
+        </div>
+
+
+        <pre>{JSON.stringify(values, 0, 2)}</pre>
+        <button type="submit">Submit</button>
+        <button type="button" onClick="form.reset">Reset</button>
+      </form>
+    )}>
+
+  </Form>
+);
+
+export default App;
