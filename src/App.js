6e607b7ae677e431d81a32c06037569638e88944
Annsysadm <annsysadm@mail.ru>
2020-04-29 00:40:36 +0300

    added form with formik

--- src/App.js
+++ src/App.js
@@ -1,16 +1,56 @@
-import React from 'react'
-
-
-
-const onSubmit =  () => {
-  window.alert(JSON.stringify(this.values, 0, 2))
-}
+import React from 'react';
+import { Formik } from 'formik';
 
 const App = () => (
   <div>
-    START
+    <h1>Anywhere in your app!</h1>
+    <Formik
+      initialValues={{ login: '', password: '' }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+        values.login = '' 
+        values.password = '' 
+      }}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+        /* and other goodies */
+      }) => (
+          <form onSubmit={handleSubmit}>
+            <input
+              type="login"
+              name="login"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.login}
+              required={true}
+            />
+            {errors.login && touched.login && errors.login}
+            <input
+              type="password"
+              name="password"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.password}
+              required={true}
+            />
+            {errors.password && touched.password && errors.password}
+            <button type="submit" disabled={isSubmitting}>
+              Submit
+          </button>
+          </form>
+        )}
+    </Formik>
   </div>
-  
-)
+);
 
 export default App;
