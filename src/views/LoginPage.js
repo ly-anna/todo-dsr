b2b6be5045ec764fd7d5dca7f3fb47bb626d9c9b
Annsysadm <annsysadm@mail.ru>
2020-05-09 16:02:15 +0300

    refactored login page

--- src/views/LoginPage.js
+++ src/views/LoginPage.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Formik } from 'formik';
+import { Formik, Field, Form } from 'formik';
 import axios from "axios";
-
+import { createBrowserHistory } from 'history';
+// import { Redirect } from 'react-router-dom'
 axios.defaults.withCredentials = true;
 
 // This will automatically send the cookie in requests. 
@@ -9,7 +10,7 @@ axios.defaults.withCredentials = true;
  values for their own domain unless withCredentials is 
  set to true before making the request.*/
 
-
+const history = createBrowserHistory();
 const LoginPage = () => (
   <div>
     <h1>This is login page!</h1>
@@ -18,54 +19,31 @@ const LoginPage = () => (
       onSubmit={(values, { setSubmitting }) => {      //что это setSubmitting?
         // const data = new FormData(event.target);
 
-        // fetch('/api/form-submit-url', {
-        //   method: 'POST',
-        //   body: data,
-        // });
+        /*Formik will set up state internally for 
+        storing user inputs through its initialValues prop, 
+        so you don’t need to initialize state from constructor anymore.*/
+
         axios.post('http://localhost:3000/api/v1/login', values)
           .then((res) => {
             console.log(res);
-            console.log(res.data);
-          }).catch((error) => {
+            console.log('res.data', res.data);
+            setSubmitting(false);
+            localStorage.setItem('user', JSON.stringify(res.data.name));
+            console.log(localStorage.getItem('user', JSON.stringify(res.data.name)));
+            if (localStorage.getItem('user')) {
+              console.log('zzzzz')
+              history.push('/');
+            }
+            // <Redirect to="/" />
+          })
+          .catch((error) => {
             console.log(error)
           })
 
       }}
-
-    // axios.post('http://localhost:4000/users/create', userObject)
-    //     .then((res) => {
-    //         console.log(res.data)
-    //     }).catch((error) => {
-    //         console.log(error)
-    //     });
-
-    // console.log(values)
-    // const url = 'http://localhost:3000/api/v1/login';
-    // const res = await fetch(url, {
-    //   method: 'POST',
-    //   body: { "login": values.login, "password": values.password },
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //     'accept': 'application/json'
-    //   },
-    //   mode: 'no-cors',
-    //   // credentials: 'include',
-
-
-    // })
-    // console.log(res)
-
-    // curl -X POST "http://localhost:3000/api/v1/login" -H "accept: application/json" -H "Content-Type: application/json" -d "{ \"login\": \"Admin\", \"password\": \"admin123\"}"
-    // {"name":"SUPERUSER","role":"admin"}
-
-    // values.login = ''
-    // values.password = ''
-    // }}
     >
       {({
         values,
-        errors,
-        touched,
         handleChange,
         handleBlur,
         handleSubmit,
@@ -73,18 +51,16 @@ const LoginPage = () => (
         handleReset,
         /* and other goodies */
       }) => (
-          <form onSubmit={handleSubmit} onReset={handleReset}>
-            <input
+          <Form onSubmit={handleSubmit} onReset={handleReset}>
+            <Field
               type="login"
               name="login"
               onChange={handleChange}
               onBlur={handleBlur}
               value={values.login}
               required={true}
-              error={touched.login && errors.login}
             />
-            {/* {errors.login && touched.login && errors.login} */}
-            <input
+            <Field
               type="password"
               name="password"
               onChange={handleChange}
@@ -92,14 +68,13 @@ const LoginPage = () => (
               value={values.password}
               required={true}
             />
-            {errors.password && touched.password && errors.password}
             <button type="submit" disabled={isSubmitting}>
               Submit
           </button>
             <button type="reset" disabled={isSubmitting}>
               Reset
           </button>
-          </form>
+          </Form>
         )}
     </Formik>
 
