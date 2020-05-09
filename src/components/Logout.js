d25b77787335db0a9ae42359a0ef7c4947459d98
Annsysadm <annsysadm@mail.ru>
2020-05-09 22:53:50 +0300

    refactored logout comp

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,17 +1,17 @@
-// import React from 'react'
+import React from 'react';
+import axios from 'axios';
 
+function logout() {
+  axios
+    .post('http://localhost:3000/api/v1/logout')
+    .then((res) => {
+      console.log(res);
+      console.log(res.data);
+      localStorage.removeItem('user');
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
 
-// function logout() {}
-
-// onClick={() => {
-//   axios
-//     .post('http://localhost:3000/api/v1/logout')
-//     .then((res) => {
-//       console.log(res);
-//       console.log(res.data);
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//     });
-// }}}
-
+export default logout;
