81c79e750cc0ddeb0e80704d7dc33f3575b4fc3b
Annsysadm <annsysadm@mail.ru>
2020-05-10 01:40:04 +0300

    added reducers

---
+++ src/redux/reducers.js
@@ -0,0 +1,26 @@
+import { userConstants } from './constants';
+
+
+const user = JSON.parse(localStorage.getItem('user'));
+const initialState = user ? { loggedIn: true, user } : {};
+
+export default function rootReducer(state = initialState, action) {
+  switch (action.type) {
+    case userConstants.LOGIN_REQUEST:
+      return {
+        loggingIn: true,
+        user: action.user,
+      };
+    case userConstants.LOGIN_SUCCESS:
+      return {
+        loggedIn: true,
+        user: action.user,
+      };
+    case userConstants.LOGIN_FAILURE:
+      return {};
+    case userConstants.LOGOUT:
+      return {};
+    default:
+      return state;
+  }
+}
