780215eade424d598249aab446be1d57559845bb
Annsysadm <annsysadm@mail.ru>
2020-05-20 00:09:10 +0300

    fix func logintoserver

--- src/redux/authentication.reducer.js
+++ src/redux/authentication.reducer.js
@@ -4,10 +4,12 @@ const user = JSON.parse(localStorage.getItem('user'));
 const initialState = user ? { loggedIn: true, user } : { loggedIn: false };
 
 function authentication(state = initialState, action) {
+  console.log('clg before initial state', initialState);
   switch (action.type) {
     case userConstants.LOGIN_REQUEST:
       return {
-        loggingIn: true,
+        // loggingIn: true,
+        loggedIn: false,
         user: action.user,
       };
     case userConstants.LOGIN_SUCCESS:
@@ -16,11 +18,13 @@ function authentication(state = initialState, action) {
         user: action.user,
       };
     case userConstants.LOGIN_FAILURE:
-      return {};
+      return {
+        loggedIn: false,
+      };
     case userConstants.LOGOUT_REQUEST:
       return { loggingOut: true };
     case userConstants.LOGOUT_SUCCESS:
-      return { loggedIn: true };
+      return { loggedIn: false };
 
     default:
       return state;
